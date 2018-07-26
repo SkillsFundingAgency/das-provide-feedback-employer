4f51235556c08cb3285b48576872c50b452e1544
Corey <corey.faulconbridge@digital.education.gov.uk>
2018-07-26 14:56:00 +0100

    Add logout controller to allow this functionlity. Previously the logout button did nothing

---
+++ src/ESFA.DAS.EmployerProvideFeedback/Controllers/LogoutController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using ESFA.DAS.EmployerProvideFeedback.Configuration.Routing;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace ESFA.DAS.EmployerProvideFeedback.Controllers
+{
+    [Route("")]
+    public class LogoutController : Controller
+    {
+        private readonly ExternalLinksConfiguration _externalLinks;
+
+        public LogoutController(IOptions<ExternalLinksConfiguration> externalLinksOptions)
+        {
+            _externalLinks = externalLinksOptions.Value;
+        }
+
+        [HttpGet, Route("logout", Name = RouteNames.Logout_Get)]
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync("Cookies");
+            await HttpContext.SignOutAsync("oidc");
+
+            return Redirect(_externalLinks.ManageApprenticeshipSiteUrl);
+        }
+    }
+}
