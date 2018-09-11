3826efaafac22bc9496f743fb1eff3e66f7a63e6
Corey <corey.faulconbridge@digital.education.gov.uk>
2018-09-11 11:54:14 +0100

    Rename providerId property to Ukprn for consistency

--- src/Employer/ESFA.DAS.ProvideFeedback.Domain.Entities/EmployerEmailDetail.cs
+++ src/Employer/ESFA.DAS.ProvideFeedback.Domain.Entities/EmployerEmailDetail.cs
@@ -8,7 +8,7 @@ namespace Esfa.Das.ProvideFeedback.Domain.Entities
         public string EmailAddress { get; set; }
         public string ProviderName { get; set; }
         public long AccountId { get; set; }
-        public long ProviderId { get; set; }
+        public long Ukprn { get; set; }
         public string UserFirstName { get; set; }
         public Guid EmailCode { get; set; }
         public DateTime? CodeBurntDate { get; set; }
