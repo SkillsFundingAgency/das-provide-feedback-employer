3dfa06f43e1c5488b0a67c5065e8ef77db26fd65
Steve O'Hara <steve.ohara@digital.education.gov.uk>
2018-09-17 12:05:27 +0100

    [PF-25] Added IOptions file for handling Azure Cosmos config

---
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback.Api/Configuration/Azure.cs
@@ -0,0 +1,13 @@
+﻿namespace ESFA.DAS.EmployerProvideFeedback.Configuration
+{
+    public class Azure
+    {
+        public string AzureCosmosEndpoint { get; set; }
+
+        public string AzureCosmosKey { get; set; }
+
+        public string DatabaseName { get; set; }
+
+        public string EmployerFeedbackCollection { get; set; }
+    }
+}
