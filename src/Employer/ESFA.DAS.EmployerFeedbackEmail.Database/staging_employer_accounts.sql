f694188c02aea384f4909213f73cddcbb5e7caa7
Corey <corey.faulconbridge@digital.education.gov.uk>
2018-09-24 14:29:21 +0100

    PF-149 DB changes to populate webjob initial data

---
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/staging_employer_accounts.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[staging_employer_accounts]
+(
+	[AccountId] INT NOT NULL, 
+    [UserRef] UNIQUEIDENTIFIER NOT NULL, 
+    [EmailAddress] NCHAR(200) NOT NULL, 
+    [FirstName] NCHAR(75) NOT NULL 
+)
