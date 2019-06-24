fd66b5b36132cf7c152567e6766f2dd6a0b4849a
Ashley-Bass <ashley.bass@education.gov.uk>
2019-06-24 13:23:09 +0100

    Revert "Cleanup of Database files"
    This reverts commit 4a7b8d2ae9e0ffe58584dcc37bdbddadc6f9790a.

---
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/CreateEmployerEmailDetails.sql
@@ -0,0 +1,23 @@
+﻿CREATE PROCEDURE [dbo].[CreateEmployerEmailDetails]
+AS
+	INSERT INTO EmployerEmailDetails
+	SELECT 
+	 NEWID() AS 'EmailUID'
+	,NEWID() AS 'EmailCode'
+	,ea.EmailAddress AS 'EmailAddress'
+	,ea.UserRef
+	,ea.FirstName
+	,ea.AccountId
+	,c.ProviderId
+	,r.Name
+	,NULL AS 'EmailSentDate'
+	,NULL AS 'CodeBurntDate'
+	,NULL AS 'ReminderEmailSentDate'
+	FROM staging_employer_accounts ea
+	INNER JOIN staging_commitment c
+	ON ea.AccountId = c.EmployerAccountId
+	INNER JOIN staging_roatp r
+	ON c.ProviderId = r.Ukprn
+	WHERE r.ProviderType = 'Main Provider'
+	AND ea.EmailAddress NOT IN (SELECT EmailAddress FROM EmployerEmailDetails)
+RETURN 0
---
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/staging_roatp.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[staging_roatp]
+(
+	[Ukprn] INT NOT NULL, 
+    [Name] NCHAR(1000) NOT NULL, 
+    [ProviderType] NCHAR(100) NOT NULL, 
+    [ParentCompanyGuarantee] BIT NOT NULL, 
+    [NewOrganisationWithoutFinancialTrackRecord] BIT NOT NULL, 
+    [StartDate] DATETIME2 NOT NULL, 
+    [ProviderNotCurrentlyStartingNewApprentices] BIT NULL 
+)
