22a4bcbfbbac831c343595ebcd44763061c0f954
Ashley-Bass <ashley.bass@education.gov.uk>
2019-06-26 11:26:17 +0100

    Script to translate AS-IS data to new style

---
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/Post Deployment/TranslateCurrentDataIntoNew.sql
@@ -0,0 +1,17 @@
+﻿EXEC UpsertUsers (SELECT UserRef, UserFirstName, EmailAddress FROM EmployerEmailDetails)
+
+EXEC UpsertProviders (SELECT Ukprn, ProviderName FROM EmployerEmailDetails)
+
+EXEC UpsertFeedback (SELECT UserRef, Ukprn, AccountId FROM EmployerEmailDetails)
+
+EXEC ResetFeedback
+
+INSERT INTO EmployerSurveyCodes (UniqueSurveyCode, UserRef, Ukprn, AccountId, BurnDate)
+VALUES (SELECT EmailUID, UserRef, Ukprn, AccountId, CodeBurntDate FROM EmployerEmailDetails)
+
+INSERT INTO EmployerSurveyHistory (UniqueSurveyCode, SentDate, EmailType)
+VALUES (SELECT EmailUID, EmailSentDate, 1 FROM EmployerEmailDetails WHERE EmailSentDate IS NOT NULL)
+
+INSERT INTO EmployerSurveyHistory (UniqueSurveyCode, SentDate, EmailType)
+VALUES (SELECT EmailUID, EmailReminderSentDate, 2 FROM EmployerEmailDetails WHERE EmailReminderSentDate IS NOT NULL)
+
