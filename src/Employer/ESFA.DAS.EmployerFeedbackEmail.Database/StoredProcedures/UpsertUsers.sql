c77ba86ee32f3e54f1361c6a9f23d72440c7dfe6
vas <vasantha.kasirajan@digital.education.gov.uk>
2019-12-16 10:39:47 +0000

    Resolved PR comment

--- src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/StoredProcedures/UpsertUsers.sql
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/StoredProcedures/UpsertUsers.sql
@@ -4,17 +4,15 @@
 	@FirstName NVarchar(150)
 AS
 BEGIN
-	IF EXISTS (Select * from dbo.Users WHERE UserRef = @UserRef)
-		BEGIN
-			UPDATE [dbo].[Users] 
-			SET [EmailAddress] = @EmailAddress,	[FirstName] = @FirstName
-			WHERE [dbo].[Users].UserRef = @UserRef;
-		END
-	ELSE
-		BEGIN
+	  UPDATE [dbo].[Users] 
+			 SET [EmailAddress] = @EmailAddress,	[FirstName] = @FirstName
+			 WHERE [dbo].[Users].UserRef = @UserRef;
+	  
+	  IF @@ROWCOUNT = 0		
+		BEGIN		
 			INSERT INTO [dbo].[Users]
-           ([UserRef],[EmailAddress],[FirstName])
-        VALUES
-           (@UserRef,@EmailAddress,@FirstName)
+				([UserRef],[EmailAddress],[FirstName])
+			VALUES
+				(@UserRef,@EmailAddress,@FirstName)
 		END
 END
