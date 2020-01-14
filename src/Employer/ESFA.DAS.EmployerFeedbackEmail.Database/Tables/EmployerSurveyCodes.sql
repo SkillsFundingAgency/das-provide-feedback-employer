73afaa83ee6a2377cd2e96e5dc680089797bf8ba
Corey <corey.faulconbridge@digital.education.gov.uk>
2020-01-14 16:14:52 +0000

    [CON-1259] Add Azure recommended indexes to bring down DTU usage

--- src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/Tables/EmployerSurveyCodes.sql
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/Tables/EmployerSurveyCodes.sql
@@ -5,3 +5,7 @@
     [BurnDate] DATETIME NULL,
 	CONSTRAINT FK_EmployerFeedbackFeedbackId FOREIGN KEY (FeedbackId) REFERENCES [dbo].[EmployerFeedback](FeedbackId),
 )
+
+GO
+
+CREATE INDEX [IX_EmployerSurveyCodes_FeedbackId] ON [dbo].[EmployerSurveyCodes] ([FeedbackId]) WITH (ONLINE = ON)
