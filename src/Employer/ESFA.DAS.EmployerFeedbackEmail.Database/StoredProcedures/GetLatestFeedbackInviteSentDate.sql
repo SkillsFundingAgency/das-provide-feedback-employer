ccd20256020a9cc446070b12659ed45cc906a29d
VasanthaKasirajan3008 <56582101+VasanthaKasirajan3008@users.noreply.github.com>
2020-10-30 12:57:33 +0000

    Con 2412 duplicate surveys (#89)
    * CON-2412-Feedback feature sending duplicate surverys per email
    
    * CON-2412 - Feedback feature sending duplicate surverys per email
    
    * CON-2412 - updated Logs
    
    * CON-2142 Duplicate Survey
    
    * CON-2412-Fix for the Logs
    
    * CON-2412-Revert the changes
    
    * CON-2412-test to stop duplicate survey code
    
    * CON-2412-Fix for  Sequence contains no elements

--- src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/StoredProcedures/GetLatestFeedbackInviteSentDate.sql
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/StoredProcedures/GetLatestFeedbackInviteSentDate.sql
@@ -2,19 +2,41 @@
 	@FeedbackId int
 AS
 	DECLARE @inviteEmailType INT = 1;
-	-- There could be more than one survey code for each feedback as they get generated every three months
-	-- hence this is ordered by history sent date and the latest top record is returned. 
-	-- As seen on production, there could be multiple survey codes with no history or history with same sent date
-	-- this is unexpected but to handle the situation the data is also sorted by survey code to get a consistent outcome 
-	SELECT 
-		TOP 1
-		EFB.FeedbackId, ESC.UniqueSurveyCode, ESH.SentDate AS InviteSentDate
-	FROM 
-		dbo.EmployerFeedback EFB
-		LEFT JOIN EmployerSurveyCodes ESC ON EFB.FeedbackId = ESC.FeedbackId
-		LEFT JOIN EmployerSurveyHistory ESH ON ESH.UniqueSurveyCode = ESC.UniqueSurveyCode
-	WHERE 
-		( ESH.EmailType IS NULL OR ESH.EmailType = @inviteEmailType )
-		AND EFB.FeedbackId = @FeedbackId
-	ORDER BY
-		ESH.SentDate DESC, ESC.UniqueSurveyCode
+	DECLARE @invitesGenerated INT
+
+	SET @invitesGenerated =  ( SELECT count(*) FROM 
+	dbo.EmployerFeedback EFB 
+	LEFT JOIN [dbo].[EmployerSurveyCodes] ESC ON EFB.FeedbackId = ESC.FeedbackId
+	LEFT JOIN [dbo].[EmployerSurveyHistory] ESH ON ESC.UniqueSurveyCode = ESH.UniqueSurveyCode
+	WHERE EFB.FeedbackId  = @FeedbackId AND ESH.SentDate IS NULL AND ESC.BurnDate IS NULL)
+
+	IF (@invitesGenerated  = 0)
+		BEGIN
+			-- There could be more than one survey code for each feedback as they get generated every three months
+			-- hence this is ordered by history sent date and the latest top record is returned. 
+			-- As seen on production, there could be multiple survey codes with no history or history with same sent date
+			-- this is unexpected but to handle the situation the data is also sorted by survey code to get a consistent outcome 
+			SELECT 
+				TOP 1
+				EFB.FeedbackId, ESC.UniqueSurveyCode, ESH.SentDate AS InviteSentDate
+			FROM 
+				dbo.EmployerFeedback EFB
+				LEFT JOIN EmployerSurveyCodes ESC ON EFB.FeedbackId = ESC.FeedbackId
+				LEFT JOIN EmployerSurveyHistory ESH ON ESH.UniqueSurveyCode = ESC.UniqueSurveyCode
+			WHERE 
+				( ESH.EmailType IS NULL OR ESH.EmailType = @inviteEmailType )
+				AND EFB.FeedbackId = @FeedbackId
+			ORDER BY
+				ESH.SentDate DESC, ESC.UniqueSurveyCode
+		END
+	ELSE
+		BEGIN		
+		
+			SELECT 
+				TOP 1 
+				EFB.FeedbackId, ESC.UniqueSurveyCode, ESH.SentDate AS InviteSentDate 
+			FROM dbo.EmployerFeedback EFB 
+				LEFT JOIN [dbo].[EmployerSurveyCodes] ESC ON EFB.FeedbackId = ESC.FeedbackId
+				LEFT JOIN  [dbo].[EmployerSurveyHistory] ESH ON ESC.UniqueSurveyCode = ESH.UniqueSurveyCode
+			WHERE EFB.FeedbackId  = @FeedbackId AND ESH.SentDate IS NULL AND ESC.BurnDate IS NULL
+		END
--- src/Employer/ESFA.DAS.ProvideFeedback.Employer.Application/SurveyInviteGenerator.cs
+++ src/Employer/ESFA.DAS.ProvideFeedback.Employer.Application/SurveyInviteGenerator.cs
@@ -26,12 +26,14 @@ namespace ESFA.DAS.ProvideFeedback.Employer.Application
 
         public async Task GenerateSurveyInvites(GenerateSurveyCodeMessage message)
         {
+            
             var feedbackId = await _employerEmailDetailRepository.UpsertIntoFeedback(message.UserRef, message.AccountId, message.Ukprn);
             _logger.LogInformation("Done upserting feedback");
 
             if (await IsNewSurveyCodeRequired(feedbackId))
             {
                 await _employerEmailDetailRepository.InsertNewSurveyForFeedback(feedbackId);
+                _logger.LogInformation($"Generated New Survey Code for feedbackId : {feedbackId} ");
             }
         }
 
@@ -39,6 +41,8 @@ namespace ESFA.DAS.ProvideFeedback.Employer.Application
         {
             var minimumLastReminderDate = DateTime.UtcNow.AddDays(-_emailSettingsConfig.InviteCycleDays);
             var feedbackLastSent = await _employerEmailDetailRepository.GetLatestFeedbackInviteSentDateAsync(feedbackId);
+            if (feedbackLastSent is null) return false;
+            _logger.LogInformation($"FeedbackId : {feedbackId}  UniqueSurveyCode : {feedbackLastSent.UniqueSurveyCode}  InviteSentDate : {feedbackLastSent.InviteSentDate}  MinimumLastReminderDate : {minimumLastReminderDate}" );
             return feedbackLastSent.UniqueSurveyCode == null || 
                 (feedbackLastSent.InviteSentDate != null && feedbackLastSent.InviteSentDate < minimumLastReminderDate);
         }
