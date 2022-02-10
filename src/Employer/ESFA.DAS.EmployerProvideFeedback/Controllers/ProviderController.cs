eb6563a82a6598342c96a046dc828e350c14fdad
Matt <matt.daniels@digital.education.gov.uk>
2022-02-10 12:25:07 +0000

    Initial commit - placeholder new page to start solving auth and shared header issues.

---
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback/Controllers/ProviderController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ESFA.DAS.EmployerProvideFeedback.Configuration.Routing;
+using ESFA.DAS.EmployerProvideFeedback.Infrastructure;
+using ESFA.DAS.EmployerProvideFeedback.ViewModels;
+using ESFA.DAS.ProvideFeedback.Data.Repositories;
+using ESFA.DAS.ProvideFeedback.Domain.Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+
+namespace ESFA.DAS.EmployerProvideFeedback.Controllers
+{
+    public class ProviderController : Controller
+    {
+        private readonly ILogger<ProviderController> _logger;
+
+        public ProviderController(
+            ILogger<ProviderController> logger
+            )
+        {
+            _logger = logger;
+        }
+
+        //[ServiceFilter(typeof(EnsureFeedbackNotSubmitted))]
+        //[Route(RoutePrefixPaths.FeedbackRoutePath, Name = RouteNames.Landing_Get)]
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+    }
+}
