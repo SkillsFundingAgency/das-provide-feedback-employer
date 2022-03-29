18a03b968ce493e23eba756821cd2ab9a00f8180
Matt <matt.daniels@digital.education.gov.uk>
2022-03-29 16:03:02 +0100

    QF-311 fix return home link

--- src/Employer/ESFA.DAS.EmployerProvideFeedback/Controllers/FeedbackSubmittedController.cs
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback/Controllers/FeedbackSubmittedController.cs
@@ -1,15 +1,24 @@
 ﻿using ESFA.DAS.EmployerProvideFeedback.Configuration.Routing;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using SFA.DAS.Employer.Shared.UI;
 
 namespace ESFA.DAS.EmployerProvideFeedback.Controllers
 {
     [Authorize]
     public class FeedbackSubmittedController : Controller
     {
+        private readonly UrlBuilder _urlBuilder;
+
+        public FeedbackSubmittedController(UrlBuilder urlBuilder)
+        {
+            _urlBuilder = urlBuilder;
+        }
+
         [HttpGet("/{encodedAccountId}/feedback-submitted", Name = RouteNames.FeedbackAlreadySubmitted)]
         public IActionResult Index(string encodedAccountId)
         {
+            ViewBag.EmployerAccountsHomeUrl = _urlBuilder.AccountsLink("AccountsHome", encodedAccountId);
             return View();
         }
     }
