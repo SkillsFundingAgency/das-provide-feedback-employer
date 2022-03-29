768296a4610e087dae4904eb4e4abce0e83294ca
Matt <matt.daniels@digital.education.gov.uk>
2022-03-29 14:16:25 +0100

    Fix unit test

--- src/Employer/UnitTests/EmployerProvideFeedback/Infrastructure/EnsureFeedbackNotSubmittedAttributeTests.cs
+++ src/Employer/UnitTests/EmployerProvideFeedback/Infrastructure/EnsureFeedbackNotSubmittedAttributeTests.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using ESFA.DAS.EmployerProvideFeedback.Configuration.Routing;
 using ESFA.DAS.EmployerProvideFeedback.Infrastructure;
 using ESFA.DAS.ProvideFeedback.Data.Repositories;
+using ESFA.DAS.ProvideFeedback.Domain.Entities.Models;
 using FluentAssertions;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Mvc.Abstractions;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Routing;
 using Moq;
+using SFA.DAS.Encoding;
 using Xunit;
 
 namespace UnitTests.EmployerProvideFeedback.Infrastructure
@@ -18,13 +20,17 @@ namespace UnitTests.EmployerProvideFeedback.Infrastructure
     {
         private readonly Mock<IEmployerFeedbackRepository> _sessionServiceMock;
         private readonly Mock<Controller> _controllerMock;
+        private readonly Mock<IEncodingService> _encodingServiceMock;
 
         public EnsureFeedbackNotSubmittedAttributeTests()
         {
             _sessionServiceMock = new Mock<IEmployerFeedbackRepository>();
             _controllerMock = new Mock<Controller>();
-            _controllerMock.Setup(mock => mock.RedirectToRoute(It.IsAny<string>())).Returns(new RedirectToRouteResult(RouteNames.FeedbackAlreadySubmitted, null));
+            _controllerMock.Setup(mock => mock.RedirectToRoute(It.IsAny<string>(), It.IsAny<object>())).Returns(new RedirectToRouteResult(RouteNames.FeedbackAlreadySubmitted, new { encodedAccountId = "ABCDEF" }));
             _sessionServiceMock.Setup(mock => mock.IsCodeBurnt(It.IsAny<Guid>())).ReturnsAsync(true);
+            _sessionServiceMock.Setup(mock => mock.GetEmployerInviteForUniqueCode(It.IsAny<Guid>())).ReturnsAsync(new EmployerSurveyInvite());
+            _encodingServiceMock = new Mock<IEncodingService>();
+            _encodingServiceMock.Setup(m => m.Encode(It.IsAny<long>(), EncodingType.AccountId)).Returns("ABCDEF");
         }
 
         [Fact]
@@ -44,7 +50,7 @@ namespace UnitTests.EmployerProvideFeedback.Infrastructure
                _controllerMock.Object);
             context.ActionArguments.Add("uniqueCode", Guid.NewGuid());
 
-            var ensureSession = new EnsureFeedbackNotSubmitted(_sessionServiceMock.Object);
+            var ensureSession = new EnsureFeedbackNotSubmitted(_sessionServiceMock.Object, _encodingServiceMock.Object);
 
             // Act
             ensureSession.OnActionExecuting(context);
