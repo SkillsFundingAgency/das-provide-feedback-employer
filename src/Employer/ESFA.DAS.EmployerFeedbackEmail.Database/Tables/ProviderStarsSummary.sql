65fcee1ac77ba435f66fd5707fe20e181412fb1d
Ian Rose <ian.rose@digital.education.gov.uk>
2022-10-03 17:37:36 +0100

    QF-662 Employer Feedback database changes

---
+++ src/Employer/ESFA.DAS.EmployerFeedbackEmail.Database/Tables/ProviderStarsSummary.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[ProviderStarsSummary](
+	[Ukprn] [bigint] NOT NULL,
+	[ReviewCount] [int] NOT NULL,
+	[Stars] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Ukprn] ASC
+))
+
+GO
+
+ALTER TABLE [dbo].[ProviderStarsSummary] ADD  DEFAULT ((0)) FOR [ReviewCount]
+GO
