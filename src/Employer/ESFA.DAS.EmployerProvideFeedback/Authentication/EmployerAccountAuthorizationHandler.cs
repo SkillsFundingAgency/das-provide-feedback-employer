bdd7650ec3a44878dc54c46fbef39d60a08c125c
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-12-20 15:20:32 +0000

    FAI-715 - Add employer account authorisation handler
    This will need changing to check roles correctly

---
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback/Authentication/EmployerAccountAuthorizationHandler.cs
@@ -0,0 +1,138 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ESFA.DAS.EmployerProvideFeedback.Configuration.Routing;
+using ESFA.DAS.EmployerProvideFeedback.Infrastructure;
+using ESFA.DAS.ProvideFeedback.Domain.Entities.Models;
+using ESFA.DAS.ProvideFeedback.Employer.ApplicationServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Newtonsoft.Json;
+
+namespace ESFA.DAS.EmployerProvideFeedback.Authentication
+{
+    public interface IEmployerAccountAuthorisationHandler
+    {
+        bool IsEmployerAuthorised(AuthorizationHandlerContext context, bool allowAllUserRoles);
+    }
+    
+    public class EmployerAccountAuthorizationHandler : AuthorizationHandler<EmployerAccountRequirement>, IEmployerAccountAuthorisationHandler
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly IAccountService _accountsService;
+        private readonly ILogger<EmployerAccountAuthorizationHandler> _logger;
+        private readonly ProvideFeedbackEmployerWebConfiguration _configuration;
+
+        public EmployerAccountAuthorizationHandler(IHttpContextAccessor httpContextAccessor, IAccountService accountsService, ILogger<EmployerAccountAuthorizationHandler> logger, IOptions<ProvideFeedbackEmployerWebConfiguration> configuration)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _accountsService = accountsService;
+            _logger = logger;
+            _configuration = configuration.Value;
+        }
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, EmployerAccountRequirement requirement)
+        {
+            if (!IsEmployerAuthorised(context, false))
+            {
+                return Task.CompletedTask;
+            }
+
+            context.Succeed(requirement);
+
+            return Task.CompletedTask;
+        }
+
+        public bool IsEmployerAuthorised(AuthorizationHandlerContext context, bool allowAllUserRoles)
+        {
+            if (!_httpContextAccessor.HttpContext.Request.RouteValues.ContainsKey(RouteValueKeys.EncodedAccountId))
+            {
+                return false;
+            }
+            var accountIdFromUrl = _httpContextAccessor.HttpContext.Request.RouteValues[RouteValueKeys.EncodedAccountId].ToString().ToUpper();
+            var employerAccountClaim = context.User.FindFirst(c=>c.Type.Equals(EmployerClaims.Account));
+
+            if(employerAccountClaim?.Value == null)
+                return false;
+
+            Dictionary<string, EmployerUserAccountItem> employerAccounts;
+
+            try
+            {
+                employerAccounts = JsonConvert.DeserializeObject<Dictionary<string, EmployerUserAccountItem>>(employerAccountClaim.Value);
+            }
+            catch (JsonSerializationException e)
+            {
+                _logger.LogError(e, "Could not deserialize employer account claim for user");
+                return false;
+            }
+
+            EmployerUserAccountItem employerIdentifier = null;
+
+            if (employerAccounts != null)
+            {
+                employerIdentifier = employerAccounts.ContainsKey(accountIdFromUrl) 
+                    ? employerAccounts[accountIdFromUrl] : null;
+            }
+
+            if (employerAccounts == null || !employerAccounts.ContainsKey(accountIdFromUrl))
+            {
+                var requiredIdClaim = _configuration.UseGovSignIn 
+                    ? ClaimTypes.NameIdentifier : EmployerClaims.UserId;
+                
+                if (!context.User.HasClaim(c => c.Type.Equals(requiredIdClaim)))
+                    return false;
+                
+                var userClaim = context.User.Claims
+                    .First(c => c.Type.Equals(requiredIdClaim));
+
+                var email = context.User.Claims.FirstOrDefault(c => c.Type.Equals(ClaimTypes.Email))?.Value;
+
+                var userId = userClaim.Value;
+
+                var result = _accountsService.GetUserAccounts(userId, email).Result;
+                
+                var accountsAsJson = JsonConvert.SerializeObject(result.UserAccounts.ToDictionary(k => k.AccountId));
+                var associatedAccountsClaim = new Claim(EmployerClaims.Account, accountsAsJson, JsonClaimValueTypes.Json);
+                
+                var updatedEmployerAccounts = JsonConvert.DeserializeObject<Dictionary<string, EmployerUserAccountItem>>(associatedAccountsClaim.Value);
+
+                userClaim.Subject.AddClaim(associatedAccountsClaim);
+                
+                if (!updatedEmployerAccounts.ContainsKey(accountIdFromUrl))
+                {
+                    return false;
+                }
+                employerIdentifier = updatedEmployerAccounts[accountIdFromUrl];
+            }
+
+            if (!_httpContextAccessor.HttpContext.Items.ContainsKey("Employer"))
+            {
+                _httpContextAccessor.HttpContext.Items.Add("Employer", employerAccounts.GetValueOrDefault(accountIdFromUrl));
+            }
+            
+            if (!CheckUserRoleForAccess(employerIdentifier, allowAllUserRoles))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private static bool CheckUserRoleForAccess(EmployerUserAccountItem employerIdentifier, bool allowAllUserRoles)
+        {
+            if (!Enum.TryParse<EmployerUserRole>(employerIdentifier.Role, true, out var userRole))
+            {
+                return false;
+            }
+
+            return true;
+
+            // TODO re-enable this line for role checking
+            //return allowAllUserRoles || userRole == EmployerUserRole.Owner;
+        }
+    }
+}
