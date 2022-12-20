3ae888f079ca4627d481b14bc4fa3a17eebb523b
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2022-12-20 12:34:55 +0000

    FAI-715 - Add post authentication handler for claims population

---
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback/Authentication/EmployerAccountPostAuthenticationClaimsHandler.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ESFA.DAS.EmployerProvideFeedback.Infrastructure;
+using ESFA.DAS.ProvideFeedback.Employer.ApplicationServices;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Newtonsoft.Json;
+using SFA.DAS.GovUK.Auth.Services;
+
+namespace ESFA.DAS.EmployerProvideFeedback.Authentication
+{
+    public class EmployerAccountPostAuthenticationClaimsHandler : ICustomClaims
+    {
+        private readonly ProvideFeedbackEmployerWebConfiguration _configuration;
+        private readonly IAccountService _accountService;
+
+        public EmployerAccountPostAuthenticationClaimsHandler(IOptions<ProvideFeedbackEmployerWebConfiguration> configuration, IAccountService accountService)
+        {
+            _configuration = configuration.Value;
+            _accountService = accountService;
+        }
+        
+        public async Task<IEnumerable<Claim>> GetClaims(TokenValidatedContext tokenValidatedContext)
+        {
+            string userId;
+            var email = string.Empty;
+            var claims = new List<Claim>();
+            
+            if (_configuration.UseGovSignIn)
+            {
+                userId = tokenValidatedContext.Principal.Claims
+                    .First(c => c.Type.Equals(ClaimTypes.NameIdentifier))
+                    .Value;
+                email = tokenValidatedContext.Principal.Claims
+                    .First(c => c.Type.Equals(ClaimTypes.Email))
+                    .Value;
+                claims.Add(new Claim(EmployerClaims.EmailAddress, email));
+            }
+            else
+            {
+                userId = tokenValidatedContext.Principal.Claims
+                    .First(c => c.Type.Equals(EmployerClaims.UserId))
+                    .Value;
+            }
+            
+            var result = await _accountService.GetUserAccounts(userId, email);
+
+            
+            var accountsAsJson = JsonConvert.SerializeObject(result.UserAccounts.ToDictionary(k => k.AccountId));
+            claims.Add(new Claim(EmployerClaims.Account, accountsAsJson, JsonClaimValueTypes.Json));
+            claims.Add(new Claim(EmployerClaims.UserId, result.EmployerUserId));
+            claims.Add(new Claim(EmployerClaims.GivenName, result.FirstName));
+            claims.Add(new Claim(EmployerClaims.FamilyName, result.LastName));
+            
+            return claims;
+        }
+    }
+}
---
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback/Authentication/EmployerClaims.cs
@@ -0,0 +1,11 @@
+namespace ESFA.DAS.EmployerProvideFeedback.Authentication
+{
+    public static class EmployerClaims
+    {
+        public static string UserId => "http://das/employer/identity/claims/id";
+        public static string EmailAddress => "http://das/employer/identity/claims/email_address";
+        public static string GivenName => "http://das/employer/identity/claims/given_name";
+        public static string FamilyName => "http://das/employer/identity/claims/family_name";
+        public static string Account => "http://das/employer/identity/claims/account";
+    }
+}
