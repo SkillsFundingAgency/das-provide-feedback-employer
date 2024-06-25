28e4cb3923c9a3dcc374efc08455367cd8b1afdb
ratheeshri1984 <ratheeshri1984@gmail.com>
2024-06-25 14:50:07 +0100

    EC-483-Class change

---
+++ src/Employer/ESFA.DAS.EmployerProvideFeedback.Api/Models/EmployerFeedbackStarsAnnualSummaryDto.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+
+namespace ESFA.DAS.EmployerProvideFeedback.Api.Models
+{
+    [Serializable]
+    public class EmployerFeedbackStarsAnnualSummaryDto
+    {
+        [JsonProperty(PropertyName = "ukprn", Order = int.MinValue)]
+        public long Ukprn { get; set; }
+
+        [JsonProperty(PropertyName = "stars", Order = int.MinValue)]
+        public int Stars { get; set; }
+
+        [JsonProperty(PropertyName = "reviewCount", Order = int.MinValue)]
+        public int ReviewCount { get; set; }
+
+        [JsonProperty(PropertyName = "timeperiod")]
+        public string TimePeriod { get; set; }
+
+        [JsonProperty(PropertyName = "providerAttribute")]
+        public IEnumerable<ProviderAttributeAnnualSummaryItemDto> ProviderAttribute { get; set; }
+    }
+}
