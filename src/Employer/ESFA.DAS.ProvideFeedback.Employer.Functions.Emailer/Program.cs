14393549666df2e84b61126cb0c18cc272a6af5a
David Rimar <david.rimar@digital.education.gov.uk>
2024-09-04 15:15:03 +0100

    program.cs

---
+++ src/Employer/ESFA.DAS.ProvideFeedback.Employer.Functions.Emailer/Program.cs
@@ -0,0 +1,88 @@
+﻿using ESFA.DAS.Feedback.Employer.Emailer.Configuration;
+using ESFA.DAS.ProvideFeedback.Data.Repositories;
+using ESFA.DAS.ProvideFeedback.Employer.Application;
+using ESFA.DAS.ProvideFeedback.Employer.ApplicationServices.Configuration;
+using ESFA.DAS.ProvideFeedback.Employer.ApplicationServices;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
+using Microsoft.Extensions.Options;
+using SFA.DAS.Notifications.Api.Client.Configuration;
+using SFA.DAS.Notifications.Api.Client;
+using System.Configuration;
+using System.IO;
+using System.Net.Http.Headers;
+using ESFA.DAS.ProvideFeedback.Employer.Functions.Emailer.Database;
+using ESFA.DAS.ProvideFeedback.Employer.Functions.Emailer.StartUpExtensions;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureAppConfiguration((hostBuilderContext, builder) => { builder.BuildDasConfiguration(hostBuilderContext.Configuration); })
+    .ConfigureServices((context, s) =>
+    {
+        var serviceProvider = s.BuildServiceProvider();
+        var configuration = context.Configuration;
+        var configBuilder = new ConfigurationBuilder()
+        .AddConfiguration(configuration)
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+        .AddEnvironmentVariables();
+        var config = configBuilder.Build();
+
+        s.AddDatabaseRegistration(config);
+        s.AddApplicationInsightsTelemetry();
+
+        s.AddLogging(options =>
+        {
+            options.AddApplicationInsights();
+            options.AddFilter<ApplicationInsightsLoggerProvider>("ESFA.DAS", Microsoft.Extensions.Logging.LogLevel.Information);
+            options.AddFilter<ApplicationInsightsLoggerProvider>("Microsoft", Microsoft.Extensions.Logging.LogLevel.Warning);
+        });
+
+        s.AddSingleton(typeof(ILogger<>), typeof(Logger<>));
+
+        s.Configure<EmployerFeedbackSettings>(config.GetSection("EmployerFeedbackSettings"));
+
+        var notificationApiConfig = config.GetSection("NotificationApi").Get<NotificationApiConfig>();
+
+        s.AddHttpClient<INotificationsApi, NotificationsApi>(c =>
+        {
+            c.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", notificationApiConfig.ClientToken);
+        });
+
+        s.AddSingleton<INotificationsApiClientConfiguration, NotificationsApiClientConfiguration>(a =>
+            new NotificationsApiClientConfiguration
+            {
+                ApiBaseUrl = notificationApiConfig.BaseUrl,
+                ClientToken = notificationApiConfig.ClientToken
+            }
+        );
+
+        s.AddTransient<IEmployerFeedbackRepository, EmployerFeedbackRepository>();
+
+        s.AddTransient<FeedbackSummariesService>();
+
+        var accApiConfig = config.GetSection("AccountApi").Get<AccountApiConfiguration>();
+        s.AddSingleton<IAccountApiConfiguration>(accApiConfig);
+        s.AddSingleton<IAccountService, AccountService>();
+        s.AddHttpClient<SecureHttpClient>();
+
+        s.Configure<RoatpApiConfiguration>(config.GetSection("RoatpApi"));
+        s.AddSingleton(cfg => cfg.GetService<IOptions<RoatpApiConfiguration>>().Value);
+        s.AddTransient<IAzureClientCredentialHelper, AzureClientCredentialHelper>();
+        s.AddHttpClient<IRoatpService, RoatpService>();
+
+        var commitmentV2ApiConfig = config.GetSection("CommitmentV2Api").Get<CommitmentApiConfiguration>();
+        s.AddSingleton<ICommitmentApiConfiguration>(commitmentV2ApiConfig);
+        s.AddSingleton<ICommitmentService, CommitmentService>();
+    })
+    .ConfigureLogging(logging =>
+    {
+        logging.AddFilter("Microsoft", LogLevel.Warning);
+    })
+    .Build();
+
+host.Run();
